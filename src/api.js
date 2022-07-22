adaf557293101b987c9e763e4c5553f93d99a88b
冯方清 <fangqing_0812@163.com>
2022-07-22 23:48:57 +0800

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'd04d3c98fdmsh529f7b01e508f7dp125eacjsnb3228de9ceda',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+}
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = '481676eee6e271ae364f71f56a711bd1'
